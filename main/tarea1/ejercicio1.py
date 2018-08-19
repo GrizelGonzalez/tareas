81e3de919e26041bc7881ff1bee5c616410250c1
grizel <mriamgonzalez859@gmail.com>
2018-08-18 23:06:19 -0500

    ->Ejercicio 1 y 3 terminado

---
+++ main/tarea1/ejercicio1.py
@@ -0,0 +1,14 @@
+print("Ingresa tu nombre: ")
+name = input()
+
+print("Ingresa tu edad: ")
+year = input()
+year = int(year)
+
+print("Ingresa tu altura: ")
+height = input()
+height = float(height)
+
+print("Tu nombre es: ", name)
+print("Tu edad es: ", year)
+print("Tu altura es: ", height)
---
+++ main/tarea1/ejercicio3.py
@@ -0,0 +1,12 @@
+print("Ingresa tu nombre: ")
+name = input()
+
+print("Ingresa tu edad: ")
+year = input()
+year = int(year)
+
+print("Ingresa tu altura: ")
+height = input()
+height = float(height)
+
+print(name,year,height)
