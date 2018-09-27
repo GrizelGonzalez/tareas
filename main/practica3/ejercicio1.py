319d4e3c04f7e7e4b1276379bb7d89ca6444c9c8
grizel <mriamgonzalez859@gmail.com>
2018-09-27 18:20:41 -0500

    ->Practica aulica tercera parte

---
+++ main/practica3/ejercicio1.py
@@ -0,0 +1,17 @@
+lista = []
+
+opc = 'y'
+while opc == 'y':
+    valor = float (input("Dame un valor"))
+    lista.append(valor)
+    print("¿Otro valor?")
+    opc = input("s = si/n = no")
+print(lista)
+sum = 0.0
+suma2 = 0.0
+for i in range(0,len(lista)):
+    sum = sum+lista[i]
+    media = sum/len(lista)
+    print("La media es:", media)
+    print("La suma de la lista es: ", sum)
+
