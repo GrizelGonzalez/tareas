8582c2e1566d79271bed75dda8c37eb46e46dc42
ignacio <ignaciomenchaca666@gmail.com>
2018-09-30 20:44:23 -0500

    1

--- main/tarea6/ejercicio1.py
+++ main/tarea6/ejercicio1.py
@@ -1,9 +1,13 @@
 lista = ["Ana", "Luis", "Pedro", "Juan"]
-print(lista)
-remover = input('Escribe el nombre que desea remover: ')
-lista.remove(remover.capitalize())
-nombre = input("Ingrese el nombre nuevo para agregar: ")
-lista.append(nombre.capitalize())
+while True:
+    print(lista)
+    remover = input('Escribe el nombre que desea remover: ')
+    lista.remove(remover.capitalize())
+    nombre = input("Ingrese el nombre nuevo para agregar: ")
+    lista.append(nombre.capitalize())
+    print(lista)
 
-print(lista)
+    opc = input('¿Desea continuar? s/n: ')
+    if opc.lower() != 's':
+        break
 
--- main/tarea6/ejercicio2.py
+++ main/tarea6/ejercicio2.py
@@ -1,6 +1,5 @@
 lista = ["Ana", "Luis", "Pedro", "Juan"]
 print(lista)
-
 nombre = input("Ingrese el nombre a eliminar : ")
 lista.remove(nombre.capitalize())
 print(lista)
